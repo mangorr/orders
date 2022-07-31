40afcf9e8cb12e3085fc8bae378f7561ffbdb747
Zhe <zf2082@nyu.edu>
2022-07-31 17:05:41 -0400

    added features on create order and fixed the web steps

--- features/steps/orders_steps.py
+++ features/steps/orders_steps.py
@@ -17,11 +17,12 @@
 """
 Order Steps
 
-Steps file for Order.feature
+Steps file for Orders.feature
 
 For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
+
 import requests
 from behave import given
 from compare import expect
